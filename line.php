a91611693df7fad12633c53600fdf9ee4cc513a0
Pinkpink1411 <43294589+Pinkpink1411@users.noreply.github.com>
2018-09-16 16:04:07 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Uafaa423fe0b04cf0f613435cf332dc1d',
+        'Userid' => 'Uafaa423fe0b04cf0f613435cf332dc1d',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
